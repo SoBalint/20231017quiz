b845622ad51d2ac9cac5125ce25dd0bde9b70480
Niki <szilviniki9@gmail.com>
2023-10-26 14:16:58 +0200

    jelezve lett hogy hányadik kérdésnél tart a játékos, a végére született egy ötlet, betűtípus átírás

---
+++ src/interfaces/scoring.ts
@@ -0,0 +1,6 @@
+export interface IScoring {
+    id: number;
+    title: string;
+    description:string;
+    image:string;
+}
--- src/pages/quiz/index.tsx
+++ src/pages/quiz/index.tsx
@@ -1,83 +1,81 @@
-import { Card, Container, Row, Col, Button} from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Card, Container, Row, Col, Button } from "react-bootstrap";
 import RootLayout from "@/app/layout";
-import importQuestions from "@/data/questions.json"
-import { useState, useEffect, use } from 'react'
+import importQuestions from "@/data/questions.json";
+import importScoring from "@/data/scoring.json";
 import { IAnswer, IQuestion } from "@/interfaces/questions";
+import { IScoring } from "@/interfaces/scoring";
 import Notiflix from "notiflix";
 import Image from "next/image";
 
 function Quiz() {
     const [questionId, setQuestionId] = useState(0);
     const [question, setQuestion] = useState({} as IQuestion);
-    //let questions = [];
     const [questions, setQuestions] = useState([] as Array<IQuestion>);
     const [results, setResults] = useState({
         good: 0,
-        bad: 0
-    })
+        bad: 0,
+    });
 
     useEffect(() => {
         setQuestions(importQuestions);
-        let num = 0;
-        num = Math.floor(Math.random() * questions.length)
+        let num = Math.floor(Math.random() * questions.length);
         setQuestionId(num);
-    }, [])
+    }, []);
 
     useEffect(() => {
-        setQuestion(questions[questionId]); // egyszerű adatszerkezet
-        // összetett adatszerkezet (tömb, objektum)
+        setQuestion(questions[questionId]);
         setQuestion((prevState) => ({
-            ...prevState, // a.a,a.b,a.c
-            ...questions[questionId]
-        }))
-    }, [questionId, questions])
+            ...prevState,
+            ...questions[questionId],
+        }));
+    }, [questionId, questions]);
 
     const answerClick = (answer: IAnswer) => {
-        //1 == "1" - true
-        //1 === "1" - false
         if (answer.id === question.correctAnswer) {
-            Notiflix.Notify.success("Jó válasz!")
+            Notiflix.Notify.success("Jó válasz!");
             setResults((prepState) => ({
                 ...prepState,
-                good: prepState.good + 1
-            }))
+                good: prepState.good + 1,
+            }));
         } else {
-            Notiflix.Notify.failure("Rossz válasz!")
+            Notiflix.Notify.failure("Rossz válasz!");
             setResults((prepState) => ({
                 ...prepState,
-                bad: prepState.bad + 1
-            }))
+                bad: prepState.bad + 1,
+            }));
         }
 
-        //questions.splice(questionId, 1);
-        setQuestions(questions.filter(q => q.id !== question.id))
-    }
-
+        setQuestions(questions.filter((q) => q.id !== question.id));
+    };
+    let point = importScoring.find((p: IScoring) =>{ return p.id === 1});
+    if ((results.bad+results.good)===5){point = importScoring.find((p: IScoring) =>{ return p.id === results.good});}
     return (
         <RootLayout>
-            {questions.length > 0 && results.good+results.bad < 5 ? (
+            {questions.length > 0 && results.good + results.bad < 5 ? (
                 <>
                     <Card.Header>
                         <h2 className="d-flex justify-content-center">{question.question}</h2>
-                        <br/>
-                        <h3>5/{results.good+results.bad}</h3>
+                        <br />
+                        <h3>5/{results.good + results.bad}</h3>
                     </Card.Header>
                     <Card.Body>
                         <Container>
                             <Row>
-                                {question.answers && question.answers.map(a => (
-                                    <Col
-                                        sm={12}
-                                        md={6}
-                                        className="text-center py-5 answer"
-                                        onClick={() => {
-                                            answerClick(a)
-                                        }}
-                                        key={a.id}
-                                    >
-                                        {a.answer}
-                                    </Col>
-                                ))}
+                                {question.answers &&
+                                    question.answers.map((a) => (
+                                        <Col
+                                            sm={12}
+                                            md={6}
+                                            className="text-center py-5 answer"
+                                            onClick={() => {
+                                                answerClick(a);
+                                            }}
+                                            key={a.id}
+                                        >
+                                            {a.answer}
+                                        </Col>
+                                    ))}
                             </Row>
                         </Container>
                     </Card.Body>
@@ -91,19 +89,24 @@ function Quiz() {
                     <Card.Body>
                         <Container>
                             <Row>
-                                <Col ms={6}> <Image src={"/logo.png"} width={200} height={200} alt="Logo"/></Col>
+                                <Col ms={6}>
+                                    <Image
+                                        src={point.image}
+                                        width={200}
+                                        height={200}
+                                        alt="Pont kép"
+                                    />
+                                </Col>
                                 <Col sm={6} className="p-2">
-                                    <h3>Jó válaszok: {results.good}</h3>
-                                    <h3>Rossz válaszok: {results.bad}</h3>
-
-                                    </Col>
-
+                                    <h3>{point.title}  5/{results.good}</h3>
+                                    <h4>{point.description}</h4>
+                                </Col>
                             </Row>
-                                <Row>
+                            <Row>
                                 <Col className="d-flex justify-content-center">
-                                    <Button id="AllButton" onClick={() => {
-                                        location.href="/"
-                                    }}>Vissza a fő oldalra</Button>
+                                    <Button id="AllButton" onClick={() => { location.href = "/"; }}>
+                                        Vissza a fő oldalra
+                                    </Button>
                                 </Col>
                             </Row>
                         </Container>
@@ -114,4 +117,4 @@ function Quiz() {
     );
 }
 
-export default Quiz
+export default Quiz;
